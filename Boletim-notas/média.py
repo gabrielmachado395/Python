3c7b65b8544f4680885b803b347c8b2c884fcca8
Gabriel Machado <gabrielmachado.emp@gmail.com>
2021-09-03 12:54:38 -0300

    Update média.py

--- Boletim-notas/média.py
+++ Boletim-notas/média.py
@@ -1,4 +1,5 @@
-#Um sistema que irá calcular a média de um aluno e no fim dirá se ele passou de ano, se ele ficou de recuperação ou se ele reprovou.
+#Um sistema que irá calcular duas notas de um aluno e formar uma média 
+#No fim dirá se ele passou de ano, se ele ficou de recuperação ou se ele reprovou.
 
 n1 = float(input('Digite a primeira nota: '))
 n2 = float(input('Digite a segunda nota: '))
